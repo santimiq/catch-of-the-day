1301f52aed911424ebd6ca34d37b423609577824
Santiago Miqueo <smiqueo@hotmail.com>
2020-05-04 16:56:55 -0500

    loading data into state onClick

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -7,7 +7,7 @@ class Inventory extends React.Component {
       <div className="AddFishForm">
       <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish} />
-
+        <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
       </div>
     )
   }
