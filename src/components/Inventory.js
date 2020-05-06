d17c507b40081d5a8ba1791fc2fade7c235f57a7
Santiago Miqueo <smiqueo@hotmail.com>
2020-05-06 12:36:34 -0500

    Removing items from sate

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -7,7 +7,13 @@ class Inventory extends React.Component {
     return (
       <div className="AddFishForm">
       <h2>Inventory</h2>
-        {Object.keys(this.props.fishes).map(key => <EditFishForm key={key} fish={this.props.fishes[key]} index={key} updateFish={this.props.updateFish} />)}
+        {Object.keys(this.props.fishes).map(key =>
+        <EditFishForm
+        key={key}
+        fish={this.props.fishes[key]}
+        index={key}
+        deleteFish={this.props.deleteFish}
+        updateFish={this.props.updateFish} />)}
         <AddFishForm addFish={this.props.addFish} />
         <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
       </div>
